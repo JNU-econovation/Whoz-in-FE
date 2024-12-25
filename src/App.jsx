811d960dc3b83d10e0f2f9d9181dd0a4d7adf8e6
gyuminzzz <gminerva@kakao.com>
2024-12-25 21:23:13 +0900

    design: 로그인/가입 페이지에서 하단 네비바 숨기기

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter, useLocation } from "react-router-dom";
 import Login from "./pages/login";
 import Join from "./pages/join";
 import Main from "./pages/main";
@@ -11,25 +11,35 @@ import MyProfile from "./pages/account/MyProfile";
 import Setting from "./pages/account/setting";
 
 const App = () => {
+  const location = useLocation();  // 현재 경로를 확인
+
+  // 로그인 또는 회원가입 페이지일 때 BottomNav를 숨기기
+  const isAuthPage = location.pathname === '/login' || location.pathname === '/join';
+
   return (
     <div className="root-wrap">
-      <BrowserRouter>
-        <Header />
-        <div className="content-wrap">
-          <Routes>
-            <Route path="/join" element={<Join />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/main" element={<Main />} />
-            <Route path="/mypage" element={<MyPage />} />
-            <Route path="/mypage/profile" element={<MyProfile />} />
-            <Route path="/mypage/device-management" element={<ManageDevice />} />
-            <Route path="/mypage/setting" element={<Setting />} />
-          </Routes>
-        </div>
-        <BottomNav />
-      </BrowserRouter>
+      <Header />
+      <div className="content-wrap">
+        <Routes>
+          <Route path="/join" element={<Join />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/main" element={<Main />} />
+          <Route path="/mypage" element={<MyPage />} />
+          <Route path="/mypage/profile" element={<MyProfile />} />
+          <Route path="/mypage/device-management" element={<ManageDevice />} />
+          <Route path="/mypage/setting" element={<Setting />} />
+        </Routes>
+      </div>
+      {/* 로그인과 회원가입 페이지에서는 BottomNav 숨기기 */}
+      {!isAuthPage && <BottomNav />}
     </div>
   );
 };
 
-export default App;
+const Root = () => (
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
+
+export default Root;
