190a91842b5dedccc42b25bdf750600c58524e42
gyuminzzz <gminerva@kakao.com>
2025-01-20 19:44:52 +0900

    refactor: 소셜 로그인 revert

--- src/pages/auth/OAuthSuccess.jsx
+++ src/pages/auth/OAuthSuccess.jsx
@@ -5,12 +5,9 @@ const OAuthSuccess = () => {
     const navigate = useNavigate();
 
     useEffect(() => {
-        // Query Parameter에서 is_registered 값 추출
+        //is_registered 추출
         const params = new URLSearchParams(window.location.search);
         const isRegistered = params.get("is-registered");
-    console.log("완료");
-       console.log(isRegistered);
-
 
         if (isRegistered === "false") {
             // 회원가입이 안 된 상태라면 추가 정보 페이지로 이동
@@ -19,7 +16,6 @@ const OAuthSuccess = () => {
         } else {
             // 이미 가입된 경우 메인 페이지로 이동
             navigate("/main");
-            console.log("true");
         }           
 
     }, [navigate]);
