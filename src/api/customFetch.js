9d8dda0ac175d2363e74292ce7daa00463142fdc
coco3x <inferior3x@gmail.com>
2025-03-02 04:45:18 +0900

    fix: JSON 변환 실패해도 예외 발생하지 않도록 수정

--- src/api/customFetch.js
+++ src/api/customFetch.js
@@ -9,16 +9,16 @@ export function customFetch(url, options = {}) {
         let data = null;
         try {
             data = await clone.json();
-        } catch (jsonError) { // JSON 변환 실패 시 텍스트 읽기
-            const errorText = await response.text();
-            console.warn("JSON 파싱 실패, 응답이 JSON이 아닐 가능성이 있음:", errorText);
-            return errorText;
+        } catch (jsonError) { // JSON 변환 실패 시 바디 출력
+            console.log("응답 바디가 JSON이 아님", errorText);
         }
+
         // 인증 필요하면 로그인 페이지로 리디렉션
         if (data?.error_code === "2001") {
             window.location.href = "/beta-login";
         }
-        return response; // JSON 데이터 반환
+
+        return response;
     })
     .catch((error) => {
         console.error("API 요청 실패:", error);
