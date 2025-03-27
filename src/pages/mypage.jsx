f13d2cd5541606a403aea1aa83365eec26e47517
coco3x <inferior3x@gmail.com>
2025-03-27 16:56:56 +0900

    update: 마이페이지에서 프로필 삭제

--- src/pages/mypage.jsx
+++ src/pages/mypage.jsx
@@ -16,7 +16,7 @@ const MyPage = () => {
             <MyPageContainer>
                 <MyProfile />
                 <ListContainer>
-                    <ListItem onClick={() => navigate('profile')}>프로필</ListItem>
+                    {/*<ListItem onClick={() => navigate('profile')}>프로필</ListItem>*/}
                     <ListItem onClick={() => navigate('device-management')}>내 기기 관리</ListItem>
                     {/* <ListItem onClick={() => navigate('setting')}>앱 설정</ListItem>  */}
                     <ListItem onClick={() => navigate('voc')}>피드백 작성하기</ListItem> 
