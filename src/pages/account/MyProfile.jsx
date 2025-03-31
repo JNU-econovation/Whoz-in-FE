077c9b2c753ecdd736d3ec2647221c7ab8b9e9bf
gyuminzzz <gminerva@kakao.com>
2025-04-01 00:00:21 +0900

    design: 기본 프로필 변경 및 프로필 컴포넌트 레이아웃 수정

--- src/pages/account/MyProfile.jsx
+++ src/pages/account/MyProfile.jsx
@@ -1,173 +1,152 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { ContentContainer } from '../../components/StyledComponents/LayoutStyles';
-import { useAuth } from '../../context/AuthContext';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser } from '@fortawesome/free-solid-svg-icons';
 import { customFetch } from '../../api/customFetch';
+import basicProfilepic from '../../components/기본프로필.png'; 
 
 const ProfileContainer = styled.div`
-    text-align: center;
-    padding: 1.25rem;
-    background: #f9f9f9;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    padding: 3rem;
     border-radius: 0.625rem;
-    box-shadow: 0 0.25rem 0.375rem rgba(0, 0, 0, 0.1);
-    padding-top: 4rem;
+    position: relative;
 `;
 
 const ProfileImageContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    position: relative;
     width: 5rem;
     height: 5rem;
-    border-radius: 50%;
-    background: #e0e0e0;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    margin: 0 auto 1.5rem;
-    overflow: hidden;
+    margin-right: 1rem;
 `;
 
 const ProfileImage = styled.img`
     width: 100%;
     height: 100%;
+    border-radius: 50%;
     object-fit: cover;
+    position: relative;
 `;
 
-const IconPlaceholder = styled(FontAwesomeIcon)`
-    font-size: 3.125rem;
-    color: #9e9e9e;
-`;
-
-const FileInputContainer = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    gap: 1rem;
-    margin-top: 1.5rem;
-`;
-
-const FileInputLabel = styled.label`
-    display: inline-block;
-    padding: 0.625rem 1.25rem;
-    font-size: 0.875rem;
+const ImageManageButton = styled.div`
+    position: absolute;
+    top: 3rem;
+    left: 3rem;
+    width: 2rem;
+    height: 2rem;
     background: #e4e4e4;
-    color: #535353;
-    border: none;
-    border-radius: 0.3125rem;
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
     cursor: pointer;
     transition: background 0.3s ease;
 
     &:hover {
-        background: #5a5791;
+        background: #999999;
     }
 `;
 
-const HiddenFileInput = styled.input`
+const FileInput = styled.input`
     display: none;
 `;
 
-const DeleteButton = styled.button`
-    padding: 0.625rem 1.25rem;
-    font-size: 0.875rem;
-    color: #fff;
-    background: #d9534f;
-    border: none;
+const Menu = styled.div`
+    position: absolute;
+    bottom: 2.5rem;
+    right: 0;
+    background: white;
+    box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
     border-radius: 0.3125rem;
+    display: flex;
+    flex-direction: column;
+    padding: 0.5rem;
+`;
+
+const MenuItem = styled.button`
+    background: none;
+    border: none;
+    padding: 0.5rem 1rem;
     cursor: pointer;
-    transition: background 0.3s ease;
+    width: 100%;
+    text-align: left;
+    transition: background 0.3s;
 
     &:hover {
-        background: #c9302c;
+        background: #f0f0f0;
     }
 `;
 
-const InfoText = styled.p`
-    margin: 0.625rem 0;
-    font-size: 1rem;
-    color: #555;
-`;
-
-const BASE_URL = process.env.REACT_APP_BACKEND_BASEURL
+const BASE_URL = process.env.REACT_APP_BACKEND_BASEURL;
 
 const MyProfile = () => {
     const [profilePic, setProfilePic] = useState(null);
     const [profileInfo, setProfileInfo] = useState({});
-  
+    const [menuOpen, setMenuOpen] = useState(false);
+
     const fetchProfile = async () => {
-      try {
-          const response = await customFetch(BASE_URL + '/api/v1/member');
-        const data = await response.json();
-          if (data.data) {
-              setProfileInfo({
-            name: data.data.name || '',
-            generation: data.data.generation !== undefined ? data.data.generation : '',
-            position: data.data.position || '',
-            statusMessage: data.data.statusMessage || '',
-          });
+        try {
+            const response = await customFetch(BASE_URL + '/api/v1/member');
+            const data = await response.json();
+            if (data.data) {
+                setProfileInfo({
+                    name: data.data.name || '',
+                    generation: data.data.generation !== undefined ? data.data.generation : '',
+                    position: data.data.position || '',
+                    statusMessage: data.data.statusMessage || '',
+                });
+            }
+        } catch (error) {
+            console.error('프로필 정보 가져오기 실패:', error);
         }
-      } catch (error) {
-        console.error('프로필 정보 가져오기 실패:', error);
-      }
     };
-  
+
     useEffect(() => {
-      fetchProfile();
+        fetchProfile();
     }, []);
-  
+
     const handleImageChange = (e) => {
-      const file = e.target.files[0];
-      if (file) {
-        const reader = new FileReader();
-        reader.onloadend = () => {
-          setProfilePic(reader.result);
-        };
-        reader.readAsDataURL(file);
-      }
+        const file = e.target.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                setProfilePic(reader.result);
+            };
+            reader.readAsDataURL(file);
+        }
+        setMenuOpen(false);
     };
-  
+
     const handleDeleteImage = () => {
-      setProfilePic(null);
+        setProfilePic(null);
+        setMenuOpen(false);
     };
-  
+
     return (
-      <ContentContainer>
-        <ProfileContainer>
-          <ProfileImageContainer>
-            {profilePic ? (
-              <ProfileImage
-                src={profilePic}
-                alt={profileInfo.name !== '정보 없음' ? `${profileInfo.name}의 프로필 이미지` : '업로드된 프로필 이미지'}
-              />
-            ) : (
-              <IconPlaceholder icon={faUser} aria-label="기본 프로필 아이콘" />
-            )}
-          </ProfileImageContainer>
-          {/*}
-          <FileInputContainer>
-            <FileInputLabel htmlFor="file-upload">
-              이미지 업로드
-            </FileInputLabel>
-            <HiddenFileInput
-              id="file-upload"
-              type="file"
-              accept="image/*"
-              onChange={handleImageChange}
-              aria-label="프로필 이미지 업로드"
-            />
-            {profilePic && (
-              <DeleteButton onClick={handleDeleteImage}>
-                삭제
-              </DeleteButton>
-            )}
-          </FileInputContainer>
-          */}
-          <b style={{ marginTop: '1rem', display: 'block' }}>{profileInfo.name}</b>
-          <InfoText>{profileInfo.generation ? `기수: ${profileInfo.generation}` : '' }</InfoText>
-          <InfoText>{profileInfo.position ? `분야: ${profileInfo.position}` : '' }</InfoText>
-          {profileInfo.statusMessage && <InfoText>{profileInfo.statusMessage}</InfoText>}
-        </ProfileContainer>
-      </ContentContainer>
+            <ProfileContainer>
+                <ProfileImageContainer>
+                    <ProfileImage src={profilePic || basicProfilepic} alt="프로필 이미지" />
+                    <ImageManageButton onClick={() => setMenuOpen(!menuOpen)}>+</ImageManageButton>
+                    {menuOpen && (
+                        <Menu>
+                            <label>
+                                <MenuItem as="span">이미지 업로드</MenuItem>
+                                <FileInput type="file" accept="image/*" onChange={handleImageChange} />
+                            </label>
+                            {profilePic && <MenuItem onClick={handleDeleteImage}>이미지 삭제</MenuItem>}
+                        </Menu>
+                    )}
+                </ProfileImageContainer>
+                <div>
+                    <b>{profileInfo.generation ? `${profileInfo.generation}기 ` : ''}{profileInfo.name}</b>
+                    <p>{profileInfo.position && `분야: ${profileInfo.position}`}</p>
+                    <div>상태메세지:{profileInfo.statusMessage && <p>{profileInfo.statusMessage}</p>}</div>
+                </div>
+            </ProfileContainer>
     );
-  };
-  
-  export default MyProfile;
+};
+
+export default MyProfile;
