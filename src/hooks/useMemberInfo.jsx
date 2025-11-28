19d18b31c19c7c94e167761a325047e3c2a6fc69
coco3x <inferior3x@gmail.com>
2025-11-29 02:40:15 +0900

    update: 캐시된 프로필 가져옴

--- src/hooks/useMemberInfo.jsx
+++ src/hooks/useMemberInfo.jsx
@@ -1,33 +1,33 @@
 import { useState, useEffect } from 'react';
 import { getMemberFromCache, fetchAndUpdateMemberCache } from '../api/storeMemberInfo';
 
-
 export const useMemberInfo = (memberId) => {
-    const [memberInfo, setMemberInfo] = useState(null);
+    const [memberInfo, setMemberInfo] = useState(() => {
+        return getMemberFromCache(memberId);
+    });
+
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState(null);
 
     useEffect(() => {
+        const cachedData = getMemberFromCache(memberId);
+        if (cachedData) {
+            setMemberInfo(cachedData);
+        }
 
         const loadData = async () => {
             setIsLoading(true);
             try {
-                // 로컬 캐시에서 이전 데이터를 먼저 읽어와 화면에 보여주기
-                console.log(memberId);
-                const cachedData = getMemberFromCache(memberId);
-                if (cachedData) {
-                    setMemberInfo(cachedData);
-                }
-
                 // 서버에 최신 정보를 요청하고 캐시를 업데이트
                 const freshData = await fetchAndUpdateMemberCache(memberId);
 
-                // 최신 정보를 화면 업데이트
+                // 서버에서 가져온 데이터가 있고, 기존 데이터와 다르다면 업데이트
                 if (freshData) {
                     setMemberInfo(freshData);
                 }
 
-                if(!cachedData && !freshData) {
+                // 캐시도 없고 서버 데이터도 없으면 에러
+                if (!cachedData && !freshData) {
                     throw new Error("사용자 정보를 불러올 수 없습니다");
                 }
 
